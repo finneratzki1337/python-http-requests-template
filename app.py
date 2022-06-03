467c3a5990241906052c32b200256562e78eb65c
finneratzki1337 <66318861+finneratzki1337@users.noreply.github.com>
2022-06-03 22:08:27 +0200

    Initial commit for requests

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from configparser import ConfigParser
 import os
 from dotenv import load_dotenv
+import requests
+from requests.auth import HTTPBasicAuth
 
 #importing own modules
 from module_template import module_class
@@ -11,16 +13,42 @@ from module_template import module_class
 config = ConfigParser()
 config.read("config/conf.conf")
 
+base_url = config['GENERAL']['BASE_URL']
+sample_file = config['GENERAL']['SAMPLE_FILE']
+
 if 'AM_I_IN_A_DOCKER_CONTAINER' not in os.environ:
     load_dotenv()
     
-user_name = os.environ['USER_NAME']
+username = os.environ['USER_NAME']
 password = os.environ['USER_PASSWORD']
-my_setting = config['GENERAL']['MY_SETTING']
 
+auth = HTTPBasicAuth(username, password)
 
 if __name__ == "__main__":
-    print(f"{user_name}/{password}")
-    print(my_setting)
-    my_class = module_class.sample_class()
-    my_class.sample_method()
+    #simple get request
+    url = f"{base_url}/"
+    response = requests.get(url)
+    print(response.text)
+
+    #simple post request
+    url_post = f"{base_url}/post/"
+    response = requests.post(url_post)
+    print(response.text)
+
+    #get request with auth
+    url_get_auth = f"{base_url}/secured/"
+    response = requests.get(url_get_auth, auth=auth)
+    print(response.text)
+
+    #post file with auth
+    url_post_file_auth = f"{base_url}/postfile/"
+    with open(sample_file, 'rb') as file:
+        bytes = file.read()
+        content = {'file_to_process' : bytes}
+    
+    response = requests.post(url_post_file_auth, files=content, auth=auth)
+    print(response.text)
+
+
+
+
--- readme.md
+++ readme.md
@@ -1,4 +1,4 @@
-# Template for Python Apps with local development running in a Docker container
+# Template for simple HTTP requests
 ##
 Pull and launch either within virtual environment
 Create virtual environment
@@ -28,6 +28,6 @@ docker-compose up --build
 ## Environment Variables
 e.g. containing credentials. .env files will be ignored by .gitignore. Copy the template file and rename to .en
 ## Config
-Folder will ignore everything but conf_template.conf. Use as needed and adjust sections. Example in template.
+Folder will ignore everything but conf_template.conf. Use as needed and adjust sections. Example in template. Especially the base_url variable for all requests
 ## Own modules
 Own modules can be imported as usual from folders in main directory. See import of module_template as example.
